f39a1f2014518ba52292932c30a3eca0b301e71e
simon <simonrenuart>
2018-11-10 15:42:13 +0100

    Exo 1-2-3

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -1,16 +1,22 @@
 package exercise1;
 
+import java.util.Scanner;
+
 public class Exercise1 {
 	static Person createPerson(String name, int age) {
-		// Ajoutez les champs name et age à la classe Person.
+		// Ajoutez les champs name et age à la classe Person. (voir fichier person.java)
 		// Créez un constructeur public permettant d'initialiser ces valeurs au moment de la construction.
 		// Créez des getters publics pour lire ces valeurs une fois la classe construite.
-		return null;
+		Person newPerson = new Person(name, age);
+		return newPerson;
 	}
 
 	public static void main(String[] args) {
 		// Considérant que cette classe est démarrée en ligne de commande avec un premier paramètre donnant le nom
 		// et un second donnant l'âge (nombre entier), créez un objet Person sur base de ceux-ci.
 		// Ecrivez ensuite le nom et l'âge de cette personne sur la sortie standard.
+
+		Person person=createPerson(args[0], Integer.parseInt(args[1]));
+		System.out.println("Veillez accueillir un nouveau né !!!! " + person.getName() + " : " + person.getAge());
 	}
 }
--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,34 @@
 package exercise2;
 
+import jdk.management.resource.internal.TotalResourceContext;
+
 class Person {
+	private int age;
+	private static int TotalAge=0;
+	private static int PopulationSize=0;
+
+
+	public Person(int a) {
+		age = a;
+		PopulationSize++;
+		TotalAge+=age;
+	}
+
+
+	public static int computePopulationSize()
+	{
+		return PopulationSize;
+	}
+
+	public static float computeAveragePopulationAge()
+	{
+		return (float) TotalAge/PopulationSize;
+	}
+
+	public static void resetPopulation()
+	{
+		TotalAge=0;
+		PopulationSize=0;
+	}
+
 }
